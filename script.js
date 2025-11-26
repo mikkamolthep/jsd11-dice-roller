a31f115ec11142bb3d3bab4006fb8e437aa65bad
mikkamolthep <mik.kamolthep@gmail.com>
2025-11-26 14:32:34 +0700

    dice roller function

---
+++ script.js
@@ -0,0 +1,77 @@
+const diceDisplay = document.getElementById("diceDisplay");
+const rollButton = document.getElementById("rollButton");
+const historyList = document.getElementById("historyList");
+const noHistoryMessage = document.getElementById("noHistoryMessage");
+
+const dotPositions = {
+  1: [{cx: 50, cy: 50}],
+  2: [{cx: 25, cy: 25}, {cx: 75, cy: 75}],
+  3: [{cx: 25, cy: 25}, {cx: 50, cy: 50}, {cx: 75, cy: 75}],
+  4: [{cx: 25, cy: 25}, {cx: 75, cy: 25}, {cx: 25, cy: 75}, {cx: 75, cy: 75}],
+  5: [{cx: 25, cy: 25}, {cx: 75, cy: 25}, {cx: 50, cy: 50}, {cx: 25, cy: 75},{cx: 75, cy: 75}],
+  6: [{cx: 25, cy: 25}, {cx: 75, cy: 25}, {cx: 25, cy: 50}, {cx: 75, cy: 50},{cx: 25, cy: 75},{cx: 75, cy: 75}],
+};
+
+// function can have parameters i.e. "value in below function"
+// possible value that this function can take are called arguments => 1,2,3,4,5,6
+
+function createDiceSVG(value) {
+  const dots = dotPositions[value] || dotPositions[1];
+  const circle = dots
+  .map (
+    (d) =>
+      `<circle cx="${d.cx}" cy="${d.cy}" r="10" fill="#4ade80"></circle>`
+  )
+  .join("");
+
+  return `
+  <svg viewbox="0 0 100 100" class="w-full h-full">
+  <rect width="100" height="100" rx"15" ry="15" fill="#333"></rect>
+  ${circle}
+  </svg>
+  `;
+}
+
+function addRollToHistory(result) {
+  if(noHistoryMessage) noHistoryMessage.remove();
+  const item = document.createElement("div");
+  item.className = "flex items-center justify-between py-2 border-b border-gray-200 text-gray-700"
+
+  const now = new Date();
+  const time = now.toLocaleDateString([], {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+  item.innerHTML = `<span class = "font-meduim">Roll: ${result}</span><span class ="text-gray-500 text-sm">${time}</span>`;
+  historyList.prepend(item)
+  if(historyList.children.length > 10) historyList.lastChild.remove();
+}
+
+function rollDice() {
+  rollButton.disabled = true
+  diceDisplay.classList.add("animate-shake");
+
+  let count = 0;
+
+  const interval = setInterval(() => {
+    const temp = Math.floor(Math.random() * 6) + 1
+    diceDisplay.innerHTML = createDiceSVG(temp)
+    if(++count >= 10) {
+      clearInterval(interval);
+      const final = Math.floor(Math.random() * 6) +1
+      diceDisplay.innerHTML = createDiceSVG(final);
+      diceDisplay.classList.remove("animate-shake");
+      rollButton.disabled = false;
+      addRollToHistory(final);
+    }
+  }, 100);
+
+}
+
+// we have prepare what we need above
+// now we will use it below
+
+rollButton.addEventListener("click",rollDice)
+
+diceDisplay.innerHTML = createDiceSVG(1)
